09f9a800349c2d465c94d5048ae60ae482d56ff3
ofir-tzier <ofir.tziter@cs.huji.ac.il>
2021-02-08 11:28:58 +0200

    add 80 tests

--- ui-tests-testng/src/test/java/com/hp/devops/demoapp/tests/ui/TestSuiteA_TestNG_Selenium.java
+++ ui-tests-testng/src/test/java/com/hp/devops/demoapp/tests/ui/TestSuiteA_TestNG_Selenium.java
@@ -66,6 +66,207 @@ public class TestSuiteA_TestNG_Selenium {
         }
     }
 
+    @Test(groups = {"Group_A"})
+    public void test1() {
+        Assert.assertEquals(1, 2);
+    }
+
+
+    @Test(groups = {"Group_A"})
+    public void test2() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test3() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test4() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test5() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test6() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test7() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test8() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test9() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test10() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test11() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test12() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test13() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test14() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test15() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test16() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test17() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test18() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test19() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test20() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test21() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test22() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test23() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test24() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test25() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test26() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test27() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test28() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test29() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test30() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test31() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test32() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test33() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test34() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test235() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test236() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test37() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test38() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test39() {
+        Assert.assertEquals(2, 2);
+    }
+
+    @Test(groups = {"Group_A"})
+    public void test40() {
+        Assert.assertEquals(2, 2);
+    }
+
     @AfterClass(alwaysRun = true)
     public void afterClass() {
         data.getDriver().quit();
